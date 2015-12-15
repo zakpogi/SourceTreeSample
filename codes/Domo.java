4b875e34c38734c5295826784051b756b702a6e3
Your name <you@example.com>
2015-12-16 00:55:35 +0800

    "added domo domo domo"

--- codes/Domo.java
+++ codes/Domo.java
@@ -2,5 +2,8 @@
 public class Domo {
 	public static void main (String[] args) {
 		System.out.println("Domo!!");
+		System.out.println("Domo!!");
+		System.out.println("Domo!!");
+		System.out.println("Domo!!");
 	}
 }
